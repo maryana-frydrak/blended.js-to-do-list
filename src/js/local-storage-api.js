57d5942eeee85086238bfdae5a8a9817531ed617
maryana-frydrak <frydra4ka@gmail.com>
2026-03-08 22:07:32 +0200

    update local-storrage

---
+++ src/js/local-storage-api.js
@@ -0,0 +1,21 @@
+export function saveTask(task) {
+  const tasks = [] || getTasks();
+  tasks.push(task);
+  const zip = JSON.stringify(tasks);
+  localStorage.setItem('tasks', zip);
+}
+
+export function getTasks() {
+  return JSON.parse(localStorage.getItem('tasks'));
+}
+
+export function innitStore() {
+  const tasks = getTasks() || [];
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+export function deleteTaskStore(id) {
+  const arr = getTasks();
+  const newArr = arr.filter(el => el.id !== id);
+  localStorage.setItem('tasks', JSON.stringify(newArr));
+}
--- src/js/markup-tasks.js
+++ src/js/markup-tasks.js
@@ -1,7 +1,11 @@
-export function createMarkup({ name, description }) {
-  return ` <li class="task-list-item">
+export function createMarkup({ name, description, id }) {
+  return ` <li class="task-list-item" data-id="${id}">
       <button class="task-list-item-btn">Delete</button>
       <h3>${name}</h3>
       <p>${description}</p>
   </li>`;
+}
+
+export function createMarkupTasks(tasks) {
+  return tasks.map(task => createMarkup(task)).join('');
 }
